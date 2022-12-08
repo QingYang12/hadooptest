25c754f4726d35f34618f0e59e9f020b7cc93513
QingYang12 <736749722@qq.com>
2022-12-08 21:28:04 +0800

    java操作hive UDF

---
+++ src/main/java/hive/udf/TestUDF.java
@@ -0,0 +1,18 @@
+package hive.udf;
+
+public class TestUDF {
+    private String str = new String();
+    public String evaluate(String str){
+        if (str == null){
+            return null;
+        }
+
+        return str+"hello";
+    }
+
+    public static void main( String[] args ) {
+        TestUDF str = new TestUDF();
+        String evaluate = str.evaluate("0000");
+        System.out.println(evaluate);
+    }
+}
